51732295db4a21c147e1f8407fdb578fb311d194
CarlosAlves3031 <alvesgt3031@gmail.com>
2023-10-19 15:58:17 -0300

    Feat: Tela Inicial

--- Projeto/database/migrations/1695952902146_replies.ts
+++ Projeto/database/migrations/1695952902146_replies.ts
@@ -1,7 +1,7 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class Comments extends BaseSchema {
-  protected tableName = 'comments'
+  protected tableName = 'replies'
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
