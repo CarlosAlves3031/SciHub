0c597a4fed4913ca1383ab1ad7dbab047e080388
CarlosAlves3031 <alvesgt3031@gmail.com>
2023-10-22 17:48:46 -0300

    Feat: Implementação dos Favoritos e Autenticação

--- Projeto/database/migrations/1695952854730_users.ts
+++ Projeto/database/migrations/1695952854730_users.ts
@@ -3,21 +3,22 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'users'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-      table.string('email').unique().notNullable()
-      table.string('password').notNullable()
+      table.increments('id').primary()
+      table.string('email', 255).notNullable().unique()
+      table.string('password', 180).notNullable()
+      table.string('remember_me_token').nullable()
 
       /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
