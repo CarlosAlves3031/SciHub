3807f2e7fa9017261c7fe017335642f0ff872b47
CarlosAlves3031 <alvesgt3031@gmail.com>
2023-10-24 12:03:51 -0300

    Fix: Tela Inicial

--- Projeto/database/migrations/1695952854731_posts.ts
+++ Projeto/database/migrations/1695952854731_posts.ts
@@ -1,7 +1,7 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class Moments extends BaseSchema {
-  protected tableName = 'post'
+  protected tableName = 'posts'
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
--- Projeto/tailwind.config.js
+++ Projeto/tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./resources/**/*.{edge,js,ts,jsx,tsx,vue}",
+    "./resources/**/*.{edge,js,ts,jsx,tsx,vue}","./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
